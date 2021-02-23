0ed737ddab99b2234694c6d20630bffe4ff3cb66
ADM-Sasha-Sasha <webi9876@gmail.com>
2021-02-23 11:35:59 +0200

    тест2

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ public class Main {
     public static void main(String[] args) {
         //sout
         System.out.println("Hello World!");
-        System.out.println("MyCode");
+        System.out.println("Привет! Меня зовут Саша");
         System.out.println("SOUT, PSVM");
         System.out.println("2 + 2 = " + (2 + 2));
         System.out.println("2 + 2 = " + 2 + 2);
