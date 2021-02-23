d3e3ce466f4cf5a5b7ecbd31078947f93202252c
ADM-Sasha-Sasha <webi9876@gmail.com>
2021-02-23 10:19:31 +0200

    test1

--- src/Main.java
+++ src/Main.java
@@ -12,6 +12,7 @@ public class Main {
         System.out.println("Finish");
         System.out.println("Finish");
         System.out.println("Finish");
+        System.out.println("Finish");
 
 
 
